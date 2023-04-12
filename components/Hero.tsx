ffde51aee41b14db081f0c72c6bf1763a7bf6a74
FASILLEAU Lucas <lfasillea@devoteam.com>
2023-04-12 13:56:09 +0200

    finish hero section start about section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
+import Link from 'next/link';
 
 type Props = {}
 
@@ -21,12 +22,33 @@ function Hero({ }: Props) {
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center
     text-center overflow-hidden'>
-      <BackgroundCircles /> 
-      <img src='' alt='' />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor='#F7AB0A' />
-      </h1>
+      <BackgroundCircles />
+      <img
+        className='relative rounded-full h-32 w-32 mx-auto object-cover '
+        src="lucas.jpeg" alt='' />
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-300 pb-2 tracking-[15px]'>Developpeur Web</h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#F7AB0A' />
+        </h1>
+
+        <div className='pt-5'>
+          <Link href="#about">
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href="#experience">
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href="skills">
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href="projects">
+            <button className='heroButton'>Projects</button>
+          </Link>
+
+        </div>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage   } from "next";
 import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 
 
@@ -19,13 +20,15 @@ const Home: NextPage = () => {
 
       {/* Hero */} 
 
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
-         </section>
-
-      
+      </section>
 
       {/* About  */}
+
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/* Experience  */}
 
